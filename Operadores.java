90ae84e43cad739beadc927c30f4a948aa32dece
m-silvera0 <migueltenato@gmail.com>
2023-02-14 14:13:03 +0000

    envio a mensagem

--- Operadores.java
+++ Operadores.java
@@ -1,3 +1,12 @@
 public class Operadores {
-    
+    public static void main(String[] miguelzzao ){
+
+        int idade;
+        idade = 16;
+        idade++;
+        idade++;
+        System.out.println("Eu tenho "+ idade +" anos");
+        idade+=10;
+        System.out.println(idade);
+    }
 }
