f5c802cee45209e063cad9190b6ce05ee47fb018
m-silvera0 <migueltenato@gmail.com>
2023-02-14 14:44:33 +0000

    envio via mensagem

--- Operadores.java
+++ Operadores.java
@@ -1,12 +1,24 @@
-public class Operadores {
-    public static void main(String[] miguelzzao ){
+import java.util.Scanner;
 
+public class Operadores {
+
+    public static void main(String[] miguelzzao ){
+        Scanner entrada = new Scanner(System.in);
         int idade;
-        idade = 16;
-        idade++;
-        idade++;
-        System.out.println("Eu tenho "+ idade +" anos");
-        idade+=10;
+        int numero1;
+        int numero2;
+        int soma;
+
+        System.out.println( "Digite sua idade");
+        idade=entrada.nextInt();
         System.out.println(idade);
+
+        System.out.println("Digite um número");
+        numero1=entrada.nextInt();
+
+        System.out.println("Digite um número");
+        numero2=entrada.nextInt();
+        soma=numero1+numero2;
+        System.out.println("A soma dos dois números é:" +soma);
     }
 }
